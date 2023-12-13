8da3238e370660bac77711abdbef4c452ad35af8
MAngeles <maria.escobar@tokiota.com>
2023-12-13 16:00:36 +0100

    Ejercicio 1.7

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -1,22 +1,60 @@
 import { useState } from 'react'
 
 
-const Button = ({handleClick,text}) => <button onClick={handleClick}>
+const Button = ({ handleClick, text }) => <button onClick={handleClick}>
   {text}
 </button>
 
-const Display = ({ counter,text }) => <div>{text}: {counter}</div>
+const Display = ({ counter, text,unity }) => <div>{text}: {counter} {unity}</div>
 
 const App = () => {
   // guarda los clics de cada botón en su propio estado
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const [total, setTotal] = useState(0)
+  const [positive, setPositive]=useState(0)
+  const [media, setMedia]=useState(0)
 
-  const setGoodFeedback = () => setGood(good + 1)
-  const setBadFeedback = () => setBad(bad + 1)
-  const setNeutralFeedback = () => setNeutral(neutral + 1)
+  const setGoodFeedback = () => {
+    const goodClick = good + 1;
+    setGood(goodClick); 
+    console.log(goodClick)
+    const totalClick=total+1;
+    setTotal(totalClick);
 
+    const positiveClick=((goodClick)/totalClick)*100;
+    setPositive(positiveClick);
+    
+    calculateMedia(neutral, goodClick, bad, totalClick, setMedia)
+  }
+  const setBadFeedback = () => {
+     const badClick = bad + 1;
+     setBad(badClick); 
+     const totalClick=total+1;
+     setTotal(totalClick);
+      
+     const goodClick=good;
+     const positiveClick=((goodClick)/totalClick)*100;
+     setPositive(positiveClick);
+     
+    calculateMedia(neutral, goodClick, badClick, totalClick, setMedia)
+    }
+  const setNeutralFeedback = () => { 
+    const neutralClick = neutral + 1;
+    setNeutral(neutralClick); 
+    const totalClick=total+1;
+    setTotal(totalClick);   
+
+    const goodClick=good;
+    const positiveClick=((goodClick)/totalClick)*100;
+    setPositive(positiveClick);
+
+    const badClick = bad;
+    
+    calculateMedia(neutral, goodClick, badClick, totalClick, setMedia)
+  }
+ 
   return (
     <div>
       <h1>Give feedback</h1>
@@ -25,11 +63,22 @@ const App = () => {
       <Button handleClick={setBadFeedback} text='bad'></Button>
 
       <h1>Statistics</h1>
-      <Display text='good' counter={good}/>
-      <Display text='neutral' counter={neutral}/>
-      <Display text='bad' counter={bad}/>
-      </div>
+      <Display text='good' counter={good} />
+      <Display text='neutral' counter={neutral} />
+      <Display text='bad' counter={bad} />
+      <hr></hr>
+
+      <Display text='all' counter={total} />
+      <Display text='average' counter={media} />
+      <Display text='positive' counter={positive} unity='%' />
+    </div>
   )
 }
 
 export default App
+
+function calculateMedia(neutral, goodClick, badClick, totalClick, setMedia) {
+  const neutralClick = neutral
+  const mediaClick = (((goodClick * 1) + (badClick * -1) + (neutralClick * 0)) / totalClick)
+  setMedia(mediaClick)
+}
